a4ed9d974c5d2b628935fec7a050320665432c0b
Evan <evanr70@gmail.com>
2021-01-23 15:28:20 +0000

    Initial commit

---
+++ tests/test_skeleton.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from py_spm.skeleton import fib
+
+__author__ = "Evan"
+__copyright__ = "Evan"
+__license__ = "mit"
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
